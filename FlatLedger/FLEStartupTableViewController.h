9aefb654e55d2751cf003afb905864bbb10015ec
MahatmaFatalError <mahatmafatal@gmail.com>
2014-01-06 13:05:34 +0100

    StartupTableViewController created

---
+++ FlatLedger/FLEStartupTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLEStartupTableViewController.h
+//  FlatLedger
+//
+//  Created by Julian Ruppel on 06.01.14.
+//  Copyright (c) 2014 Julian Ruppel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLEStartupTableViewController : UITableViewController
+
+@end
